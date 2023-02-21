9e9505317c2eb7a8dce43fe0f52de941cddff678
KhaledSean <khaled.sean06@gmail.com>
2023-02-21 16:05:00 +0100

    init project

---
+++ Back/tpalt/src/main/java/com/tpalt/tpalt/TpaltApplication.java
@@ -0,0 +1,13 @@
+package com.tpalt.tpalt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpaltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpaltApplication.class, args);
+	}
+
+}
---
+++ Back/tpalt/src/test/java/com/tpalt/tpalt/TpaltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpalt.tpalt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpaltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
