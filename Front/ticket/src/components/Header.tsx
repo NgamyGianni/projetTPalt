13dbc2f3c3479abb168e5e08a4e0c605deef71cd
NgamyGianni <spyker97160@hotmail.fr>
2023-02-21 17:57:00 +0100

    update header

--- Front/ticket/src/components/Header.tsx
+++ Front/ticket/src/components/Header.tsx
@@ -1,17 +1,39 @@
 import { useState } from 'react'
-import { Navbar } from "@nextui-org/react";
+import { Navbar, Button } from "@nextui-org/react";
 
 const Header = () => {
-  const [count, setCount] = useState(0)
+  const [choice, setChoice] = useState(0);
+  const [state, setState] = useState({
+    "isConnected" : true
+  })
 
   return (
     <Navbar isBordered variant={"floating"}>
         <Navbar.Content hideIn="xs">
-          <Navbar.Link href="#">Features</Navbar.Link>
-          <Navbar.Link isActive href="#">Customers</Navbar.Link>
-          <Navbar.Link href="#">Pricing</Navbar.Link>
-          <Navbar.Link href="#">Company</Navbar.Link>
+          <Navbar.Link isActive={choice==0} href="#" onClick={() => setChoice(0)}>Features</Navbar.Link>
+          <Navbar.Link isActive={choice==1} href="#" onClick={() => setChoice(1)}>Customers</Navbar.Link>
+          <Navbar.Link isActive={choice==2} href="#" onClick={() => setChoice(2)}>Pricing</Navbar.Link>
+          <Navbar.Link isActive={choice==3} href="#" onClick={() => setChoice(3)}>Company</Navbar.Link>
         </Navbar.Content>
+          {
+            !state.isConnected ?
+              <Navbar.Content>
+                <Navbar.Link color="inherit" href="#">
+                  Login
+                </Navbar.Link>
+                <Navbar.Item>
+                  <Button auto flat href="#">
+                    Sign Up
+                  </Button>
+                </Navbar.Item>
+              </Navbar.Content>
+              : 
+              <Navbar.Content>
+                <Navbar.Link color="inherit" href="#">
+                  Panier
+                </Navbar.Link>
+              </Navbar.Content>
+          }
     </Navbar>
   )
 }
