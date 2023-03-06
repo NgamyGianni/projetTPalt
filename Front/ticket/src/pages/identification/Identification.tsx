bdc0851165fff8d58c20c51e4aa32c3b3fa7b183
BLADINE <59175711+BLADINE@users.noreply.github.com>
2023-03-06 14:02:36 +0100

    authentification

--- Front/ticket/src/pages/identification/Identification.tsx
+++ Front/ticket/src/pages/identification/Identification.tsx
@@ -6,13 +6,20 @@ function Identification() {
   const [userPassword,setuserPassword] = useState("")
   return (
     <div className='identification'>
-      <div>Indentification</div>
-      <form action="">
-        <input type="email" onChange={e=>setUserMail(e.target.value)} value={userMail}/>
-        <input type="password" onChange={e=>setuserPassword(e.target.value)} value={userPassword}/>
+      <div className='title'>Authentification</div>
+      <form action="" method='post'>
+        <div>
+          <label htmlFor="mail"> Mail </label>
+          <input type="email" onChange={e=>setUserMail(e.target.value)} value={userMail}/>
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" onChange={e=>setuserPassword(e.target.value)} value={userPassword}/>
+        </div>
+        <input type="submit" value="submit" className='sumbit'/>
       </form>
-      <div>the value is </div>
-      <div>{userMail}</div>
+      {/* <div>the value is </div>
+      <div>{userMail}</div> */}
     </div>
   )
 }
