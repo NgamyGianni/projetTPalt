f7aec74142a4256be556fe18fcbf1e774fe382be
BLADINE <59175711+BLADINE@users.noreply.github.com>
2023-03-20 22:54:49 +0100

    add reservation.css in the reservation component

--- Front/ticket/src/pages/identification/Reservation.tsx
+++ Front/ticket/src/pages/identification/Reservation.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
-
+import { cinemaList } from '../../components/cinema/Cinemalist'
+import './reservation.css'
 function Reservation() {
   return (
-    <div className='reservation-list'>
+    <div className='reservationList'>
       <div className='reservation-item'>
         <div className='qr-code'> Qr Code</div>
         <div className='ticket-details'>
           <div className='movie-name'> SpiderMan</div>
           <div className='detailled-movie'> 
-            <img  src="" alt="" />
+            <img  src={cinemaList[0].img} alt="" />
             <div className='state-movie'></div>
           </div>
-          <div className='movie-name'> SpiderMan</div>
+          <div className='displaying-date'> SpiderMan</div>
         </div>
       </div>
       
